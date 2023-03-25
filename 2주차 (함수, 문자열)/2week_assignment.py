975d160a2bee53d0da2c4bfb453d30be41beab2e
ludysw124 <ludysw24@gmail.com>
2023-03-26 00:36:07 +0900

    2week_assinment

---
+++ 2주차 (함수, 문자열)/2week_assignment.py
@@ -0,0 +1,45 @@
+# 1번
+def sum(a, b):
+    # your code
+    return
+
+# 2번
+def sub(a, b):
+    # your code
+    return
+
+# 3번
+def mul(a, b):
+    # your code
+    return
+
+# 4번
+def div(a, b):
+    # your code
+    return
+
+# 5번
+def distance(x1, y1, x2, y2):
+    # your code
+    return
+
+# 6번
+def title():
+    lylics = "Switch sides and I'm beside you."
+    # your code
+    return 
+
+# 7번
+def reverseStr(string):
+    # your code
+    return 
+
+# 8번
+def introduce():
+    # your code
+    return 
+
+# 9번
+def calc():
+    # your code
+    return 
