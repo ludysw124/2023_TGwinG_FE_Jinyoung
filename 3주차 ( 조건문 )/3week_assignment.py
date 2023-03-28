e68f35c566e9cdd03ab69f206acfe8cab0c92ee7
ludysw124 <ludysw24@gmail.com>
2023-03-28 21:09:00 +0900

    3week_assignment

---
+++ 3주차 ( 조건문 )/3week_assignment.py
@@ -0,0 +1,24 @@
+# 1번
+def grade(score):
+    #your code
+    return
+
+# 2번
+def quadrant(x, y):
+    #your code
+    return
+
+# 3번
+def leapYear(year):
+    # your code
+    return
+
+# 4번
+def dice(a, b, c):
+    # your code
+    return
+
+# 5번
+def alarm(time):
+    # your code
+    return
