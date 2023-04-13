3e4afd867e1409e9ae987aab6747ea1b4d76d75f
ludysw124 <ludysw24@gmail.com>
2023-04-13 22:47:17 +0900

    5week_assignment

---
+++ 5주차 ( 모듈 )/5week_assignment.py
@@ -0,0 +1,37 @@
+# your code 를 지우고 코드를 작성하세요.
+# 5주차 과제에는 출력이나 입력을 요구하는 문제가 없습니다. print(), input() 사용하지 마세요.
+# 코드 실행 시 컴파일 에러, 런타임 에러가 발생하면 해당 문제 0점 처리하겠습니다.
+# 함수 이름 변경하지 마세요. 변경 시 해당 문제 0점 처리 하겠습니다.
+# result, answer 변수를 사용하여 문제를 풀이하세요. 반환값은 result나 answer 변수입니다.
+# 제출 기한: 2023년 4월 17일 23시 59분
+# 지각 제출 기한: 2023년 4월 18일 23시 59분
+
+def calcCircleArea(r):
+    result = float()
+    # your code
+    return result
+
+def calcLog(a, b):
+    result = float()
+    # your code
+    return result
+
+def calcSin(x):
+    result = float()
+    # your code
+    return result
+
+def calcFactorial(x):
+    result = int()
+    # your code
+    return result
+
+def calcCombination(n, r):
+    result = int()
+    # your code
+    return result
+
+def calculator(order):
+    answer = 0
+    # your code
+    return answer
